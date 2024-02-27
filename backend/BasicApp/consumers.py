5fec02bbdb331b4dcaf267752eb2b1b32b5fb4a0
alan <yatesalan4@gmail.com>
2024-02-27 08:38:55 +0200

    v2

---
+++ backend/BasicApp/consumers.py
@@ -0,0 +1,41 @@
+# from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+# class TrackConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         await self.channel_layer.group_add('BasicApp', self.channel_name)
+#         await self.accept()
+
+
+import json
+from channels.generic.websocket import WebsocketConsumer
+from .models import PageVisit
+
+
+class TrackConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Update tracking data when WebSocket connection is closed
+        pass
+
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        if data['type'] == 'visit':
+            # Create a new PageVisit instance
+            PageVisit.objects.create(
+                user_id=data['user_id'],
+                page_id=data['page'],
+                start_time=data['timestamp']
+            )
+        elif data['type'] == 'leave':
+            # Update the PageVisit instance with stop_time
+            visit = PageVisit.objects.filter(user_id=data['user_id'], page_id=data['page']).last()
+            if visit:
+                visit.stop_time = data['timestamp']
+                visit.save()
+
+    def send(self, text_data):
+        # Send message to WebSocket
+        self.send(text_data=text_data)
