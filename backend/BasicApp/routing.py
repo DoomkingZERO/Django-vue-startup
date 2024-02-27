83b315ef056ee2033b0897506f8f54e858d61787
alan <yatesalan4@gmail.com>
2024-02-27 11:16:56 +0200

    v333333333333

--- backend/BasicApp/routing.py
+++ backend/BasicApp/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from BasicApp import consumers
 
 ws_urlpatterns = [
-    path('ws/page_tracking/', consumers.PageVisitConsumer),
+    path('ws/page_tracking/', consumers.PageVisitConsumer.as_asgi()),
 ]
